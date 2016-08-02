a3ec9bed8c598cf73c4cbb9b3e1f9bae32b64c67
Ishaan Parikh <iparikh@Ishaans-MacBook-Pro.local>
2016-08-02 13:15:46 -0700

    to run every 15 min update

--- runner.py
+++ runner.py
@@ -2,6 +2,7 @@ from structures import Word
 from structures import MainMap
 from tweet_engine import tweetdb, twitter_handler
 from datetime import date
+import time
 
 def capitalize_first_word(self):
     self[0] = self[0].title()
@@ -13,23 +14,33 @@ def process_all_tweets(map, tweets):
     for sent in tweets:
         map.process_sentence(sent)
 
+def do_it():
+    tweets = twitter_handler.get_tweets()
+    map = MainMap()
+    process_all_tweets(map, tweets)
+    first_word = map.generate_first_word()
+    currWord = map.generate_next(first_word)
+    char_count = len(currWord)
+    new_sentence = []
+    while currWord is not 'ENDOFSENTENCE':
+        if not ('http' in currWord):
+            new_sentence.append(currWord)
+            currWord = map.generate_next(currWord)
+            char_count += len(currWord)
+            if '.' in currWord:
+                break
 
-tweets = twitter_handler.get_tweets()
-map = MainMap()
-process_all_tweets(map, tweets)
-first_word = map.generate_first_word()
-currWord = map.generate_next(first_word)
-char_count = len(currWord)
-new_sentence = []
-while currWord is not 'ENDOFSENTENCE':
-#    print currWord
-    if not ('http' in currWord):
-        new_sentence.append(currWord)
-        currWord = map.generate_next(currWord)
-        char_count += len(currWord)
-        if '.' in currWord:
-            break
+    capitalize_first_word(new_sentence)
+    final_sentence = turn_arr_to_sentence(new_sentence)
+    twitter_handler.publish_tweet(final_sentence)
 
-capitalize_first_word(new_sentence)
-final_sentence = turn_arr_to_sentence(new_sentence)
-print final_sentence
+
+while True:
+    do_it()
+    time.sleep(300)
+    print '10 more minutes till tweet.'
+    time.sleep(300)
+    print '5 more minutes till tweet.'
+    time.sleep(240)
+    print '1 more minutes till tweet.'
+    time.sleep(60)
